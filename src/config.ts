7458509f7836eb82a5e751cdc27c20fdca61ebe3
Гулевич Екатерина <meteorgul@gmail.com>
2025-09-18 20:31:26 +0300

    add db

--- src/config.ts
+++ src/config.ts
@@ -2,6 +2,7 @@ import dotenv from 'dotenv';
 dotenv.config();
 
 export const config = {
+    dbUrl: process.env.DATABASE_URL as string,
     roundDuration: parseInt(process.env.ROUND_DURATION || '60') * 1000,
     cooldownDuration: parseInt(process.env.COOLDOWN_DURATION || '30') * 1000,
 };
--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,11 @@
-console.log('server started');
+import sequelize from './db';
+
+const start = async () => {
+    try {
+        await sequelize.authenticate();
+        console.log('DB connected');
+    } catch (e) {
+        console.error('DB error:', e);
+    }
+};
+start();
